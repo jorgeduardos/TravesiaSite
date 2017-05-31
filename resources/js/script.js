7b6ade5bc2419d7608e2f5e32a7b668b3c99bc30
jorgeduardos <jorgeduardo.1994@gmail.com>
2017-05-30 20:08:23 -0500

    added slider

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,4 +12,43 @@ $(document).ready(function(){
 		}
 
 	});
+
+	/* SLIDER */
+
+	  var innerSliderW = $(".innerSlider").first().width();
+	  console.log(innerSliderW);
+	  //number of inner sliders
+	  var innerSliderCount = innerSliderCounter($(".innerSlider"));
+	  console.log(innerSliderCount);
+	    
+	  // slider width
+	  $(".slider").eq(0).css("width", innerSliderW*innerSliderCount);
+	  console.log($(".slider").eq(0).css("width"));
+	  
+	  //innerSlider width
+	  var slider = $(".slider").eq(0);
+	  var sliderW = slider.width();
+	  
+	  var pixelsToMove = sliderW/innerSliderCount;
+	  console.log(pixelsToMove);
+	  //
+	  console.log((innerSliderCount-1)*pixelsToMove*-1);
+	  $("#button").on("click", function(){
+	    console.log(parseInt($(".slider").first().css("marginLeft")));
+	    if(parseInt($(".slider").first().css("marginLeft")) !== (innerSliderCount-1)*pixelsToMove*-1){
+	      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
+	    }else{
+	      $(".slider").first().css("marginLeft", "0px");
+	      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
+	    }  
+	  });  
+
 });
+
+function innerSliderCounter(objectArray){
+  var count;
+  for(var i = 0; i <= objectArray.length; i++){
+   count = i;
+  }
+  return count;
+}
