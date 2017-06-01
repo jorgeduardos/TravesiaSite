91ca387ccbd2c5f6081eecbc326d49f93f28dd6e
jorgeduardos <jorgeduardo.1994@gmail.com>
2017-05-31 23:30:32 -0500

    finished slider, changed header bckg img, change logo color on header

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,46 +15,69 @@ $(document).ready(function(){
 
 	/* SLIDER */
 
-	  var innerSliderW = $(".innerSlider").first().width();
-	  console.log(innerSliderW);
-	  //number of inner sliders
-	  var innerSliderCount = innerSliderCounter($(".innerSlider"));
-	  console.log(innerSliderCount);
-	    
-	  // slider width
-	  $(".slider").eq(0).css("width", innerSliderW*innerSliderCount);
-	  console.log($(".slider").eq(0).css("width"));
-	  
-	  //innerSlider width
-	  var slider = $(".slider").eq(0);
-	  var sliderW = slider.width();
-	  
-	  var pixelsToMove = sliderW/innerSliderCount;
-	  console.log(pixelsToMove);
-	  //
-	  console.log((innerSliderCount-1)*pixelsToMove*-1);
-	  $("#button").on("click", function(){
-	    console.log(parseInt($(".slider").first().css("marginLeft")));
-	    if(parseInt($(".slider").first().css("marginLeft")) !== (innerSliderCount-1)*pixelsToMove*-1){
-	      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
-	    }else{
-	      $(".slider").first().css("marginLeft", "0px");
-	      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
-	    }  
-	  });
-
-	  // $(".ion-ios-arrow-left").on("click", function(){
-	  //   console.log(parseInt($(".slider").first().css("marginLeft")));
-	  //   if(parseInt($(".slider").first().css("marginLeft")) !== (innerSliderCount-1)*pixelsToMove*-1){
-	  //     $(".slider").animate({marginLeft: "+=" + pixelsToMove + "px"});
-	  //   }else{
-	  //     $(".slider").first().css("marginLeft", "0px");
-	  //     $(".slider").animate({marginLeft: "+=" + pixelsToMove + "px"});
-	  //   }  
-	  // });
-
+	  //width of each individual slide
+  var innerSliderW = $(".innerSlider").first().width();
+  console.log("innerSlider width = " + innerSliderW);
+  
+  
+  //number of slides
+  var innerSliderCount = innerSliderCounter($(".innerSlider"));
+  console.log("innerSlider count = " + innerSliderCount);
+    
+  //seting slider to have correct width
+  $(".slider").eq(0).css("width", innerSliderW*innerSliderCount);
+  console.log("slider width = " + $(".slider").eq(0).css("width"));
+  
+  //slider width
+  var slider = $(".slider").eq(0);
+  var sliderW = slider.width();
+  
+  //pixels to move when clicking the buttons
+  var pixelsToMove = sliderW/innerSliderCount;
+  console.log("pixelsToMove = " + pixelsToMove);
+  
+  console.log((innerSliderCount-1)*pixelsToMove*-1);
+  
+  //setting slider to show first slide (wich is technically not the first)
+  $(".slider").first().css("marginLeft", -pixelsToMove);
+  
+  //handling events
+  $("#right").on("click", function(){
+    if(parseInt($(".slider").first().css("marginLeft")) !== (innerSliderCount-1)*pixelsToMove*-1){
+      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
+      console.log(parseInt($(".slider").first().css("marginLeft")));
+    }else{
+      $(".slider").first().css("marginLeft", -pixelsToMove);
+      $(".slider").animate({marginLeft: "-=" + pixelsToMove + "px"});
+    }  
+  }); 
+  
+    $("#left").on("click", function(){
+    console.log(parseInt($(".slider").first().css("marginLeft")));
+    if(parseInt($(".slider").first().css("marginLeft")) !== 0){
+      $(".slider").animate({marginLeft: "+=" + pixelsToMove + "px"});
+    }else{
+      $(".slider").first().css("marginLeft", (innerSliderCount-2)*pixelsToMove*-1);
+      $(".slider").animate({marginLeft: "+=" + pixelsToMove + "px"});
+    }  
+  }); 
+  
+  
+  
+ //not working fully yet **************************************
+ $(window).on("resize", function(){
+   innerSliderW = $(".innerSlider").first().width();
+   console.log("innerSlider width after resize = " + innerSliderW);
+    $(".slider").eq(0).css("width", innerSliderW*innerSliderCount);
+   sliderW = $(".slider").eq(0).width();
+    console.log("slider width after resize = " + $(".slider").eq(0).css("width"));
+   pixelsToMove = sliderW/innerSliderCount;
+   console.log("pixelsToMove after resize = " + pixelsToMove)
+   
+ });
 });
 
+//little function to count number of slides in the page
 function innerSliderCounter(objectArray){
   var count;
   for(var i = 0; i <= objectArray.length; i++){
